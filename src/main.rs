dd987dead8da331304c4e77ce244d00c6e64ab7e
Muhammad Ijlal <ijlal.tanveer294@gmail.com>
2025-04-17 14:23:00 +0500

    updated exercise

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 fn main(){
-    let season = "summer";
-
-    let ans = match season {
-        "spring" => {1;},
-        "summer" => println!("It's summer!"),
-        _ => println!("It's not spring or summer!"),
-    };
+    let some_num = 100;
+    let mut _some_new_num = some_num;
 }
