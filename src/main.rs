79f4d350e249e55adae2112e77250015febeffe8
Muhammad Ijlal <ijlal.tanveer294@gmail.com>
2025-04-21 22:18:47 +0500

    completed exercise

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-
-fn main() {
-    let mut some_arr = [1, 2, 3, 4, 5];
-    some_arr[0] = 10;
-    println!("The first element is: {}", some_arr[0]);
-
+#[derive(Debug)]
+enum Tier {
+    Gold,
+    Silver,
+    Platinum,
 }
 
+enum Subscription {
+    Free,
+    Basic(f64, u32), // price per month, num of months
+    Premium { tier: Tier },
+}
+
+impl Subscription {
+    fn summarize(&self) {
+        match self {
+            Subscription::Free => println!("You have limited access to the site"),
+            Subscription::Basic(price, months) => println!(
+                "You have limited access to the site's premium features for {price} for {months} months"
+            ),
+            Subscription::Premium { tier } => println!(
+                "You have full access to the site's premium features. Your tier is {tier:?}"
+            ),
+        }
+    }
+}
+
+fn main() {
+    let instance_1 = Subscription::Free;
+    let instance_2 = Subscription::Basic(9.99, 12);
+    let instance_3 = Subscription::Premium { tier: Tier::Gold };
+
+    instance_1.summarize();
+    instance_2.summarize();
+    instance_3.summarize();
+    
+}
