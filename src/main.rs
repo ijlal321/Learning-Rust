87966f50bf1aaaf45f43255009f4ebff2cc47a2c
Muhammad Ijlal <ijlal.tanveer294@gmail.com>
2025-04-23 21:25:24 +0500

    started assignment Options

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,29 @@
-#[derive(Debug)]
-enum DigitalContent {
-    AudioFile,
-    VideoFile
-}
-
-#[derive(Debug)]
-struct ChatMessage<T> {
-    content: T,
-    time: String,
-}
-
-impl ChatMessage<DigitalContent> {
-
-    fn consume_entertainment(&self) {
-        println!("Watching the {:?}", self.content)
-    }
-}
-
-impl <T> ChatMessage<T> {
-    fn retrieve_time(&self) -> String {
-        self.time.clone()
-    }
-}
-
-
 fn main() {
-    let ChatMessage = ChatMessage {
-        content: "Hello world",
-        time: "12:00".to_string(),
-    };
+    let my_option = MyOption::Some("hello");
+    let none_option = MyOption::<i32>::None;
 
-    let ChatMessage = ChatMessage {
-        content: String::from("Hello world"),
-        time: "12:00".to_string(),
-    };
+    println!("{:?}  ,  {:?}", my_option, none_option);
 
-    let ChatMessage = ChatMessage {
-        content: DigitalContent::AudioFile,
-        time: "12:00".to_string(),
-    };
+    let my_option_res = my_option.unwrap("not working");
+    let none_res = none_option.unwrap(11);
+
+    println!("{}.", my_option_res);
+    println!("{}.", none_res);
     
+}
 
-    ChatMessage.consume_entertainment();
-    println!("The time is {}", ChatMessage.retrieve_time());
+impl <T> MyOption<T> {
+    fn unwrap(self, default_res: T) -> T {
+        match self {
+            MyOption::Some(value) => value,
+            MyOption::None => default_res
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+enum MyOption<T> {
+    Some(T),
+    None
 }
