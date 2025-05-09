e40528407f13d3b7a44f95939a481db1313af423
Muhammad Ijlal <ijlal.tanveer294@gmail.com>
2025-05-09 18:26:09 +0500

    traits asignment done

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,123 @@
-use std::fmt::Error;
-use std::fs;
-use std::io::{self, stdin};
-use std::process;
+#[allow(unused)]
+use std::fmt::{Debug, Display, Formatter, Result};
+use std::{fs::canonicalize, path::PrefixComponent};
 
-fn main() {
-    let res = write_to_file();
-    match res {
-        Ok(..) => println!("Successfully done"),
-        Err(error) => {
-            eprintln!("Some error occured: {}", error);
-            process::exit(1);
+trait Drinkable {
+    fn consume(&mut self);
+    fn get_data(&self) -> String;
+    fn stats(&self) {
+        println!("{}", self.get_data());
+    }
+}
+
+#[derive(Debug)]
+enum Milk {
+    Whole,
+    Oat,
+    Almond,
+}
+
+struct Coffee<T> {
+    kind: T,
+    Milk: Milk,
+    ounces: u32,
+}
+
+impl<T> Coffee<T> {
+    fn new(kind: T) -> Self {
+        Coffee {
+            kind,
+            Milk: Milk::Almond,
+            ounces: 100,
         }
     }
 }
 
-fn write_to_file() -> Result<String, io::Error> {
-    println!("What file would you like to write to, Master -?");
-    let mut file_name = String::new();
-    stdin().read_line(&mut file_name)?;
+impl<T: Display> Drinkable for Coffee<T> {
+    fn consume(&mut self) {
+        self.ounces = 0;
+    }
+    fn get_data(&self) -> String {
+        format!("A delicious {} ounce of {}", self.ounces, self.kind)
+    }
+}
 
-    println!("What would you like to write to the file, Master -?");
-    let mut data_to_write = String::new();
-    stdin().read_line(&mut data_to_write)?;
+impl<T: Debug> Debug for Coffee<T> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        write!(
+            formatter,
+            "Your Coffee Sire:: {:?} {:?} {:?}",
+            self.kind, self.Milk, self.ounces
+        )
+    }
+}
+#[derive(Debug)]
 
-    fs::write(file_name.trim(), data_to_write.trim())?;
+struct Soda {
+    calories: u32,
+    price: f64,
+    flavor: String,
+    percentage: u32,
+}
 
-    Ok(file_name)
+impl Soda {
+    fn new() -> Self {
+        Soda {
+            calories: 100,
+            price: 100.25,
+            flavor: "hello world".to_string(),
+            percentage: 100,
+        }
+    }
+}
+
+impl Drinkable for Soda {
+    fn consume(&mut self) {
+        self.percentage = 0;
+    }
+    fn get_data(&self) -> String {
+        format!("Flavor: {}, Calories: {}", self.flavor, self.calories)
+    }
+}
+
+impl Display for Soda {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        write!(formatter, " {} Soda ", self.flavor)
+    }
+}
+
+impl Clone for Soda{
+    fn clone(&self) -> Self {
+        Soda { calories: self.calories, price: self.price, flavor: self.flavor.clone(), percentage: self.percentage }
+    }
+}
+
+impl PartialEq for Soda{
+    fn eq(&self, other: &Self) -> bool {
+        self.price == other.price
+    }
+}
+
+impl Eq for Soda{
+
+}
+
+
+fn main() {
+    let mut latte = Coffee::new("hello world");
+    println!("{:?}", latte);
+    latte.consume();
+    println!("{:?}", latte);
+
+    let cappuccino = Coffee::new("Hello WOrld".to_string());
+    println!("{}", cappuccino.get_data());
+
+
+    let pepsi = Soda::new();
+    println!("{}", pepsi);
+    let mut coke = pepsi.clone();
+    coke.consume();
+
+    println!("{:?}", coke);
+
 }
